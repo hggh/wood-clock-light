44c89a9891b3cebd2e507ee1ed4fbca1e58d854b
Jonas Genannt <jonas@brachium-system.net>
2017-07-28 15:41:17 +0200

    initial wood clock

---
+++ config.h
@@ -0,0 +1,14 @@
+#define DEBUG true
+
+/*
+ * PIN 4, PD 2
+ */
+#define DCF_PIN 2
+#define DCF_INTERRUPT 0
+
+/*
+ * PIN 11, PD 5
+ */
+#define LED_PIN 9
+#define LED_COUNT 85
+
---
+++ display.h
@@ -0,0 +1,110 @@
+
+const static uint8_t numbers[10][15] = {
+  { // 0
+  1,1,1,1,1,
+  1,0,0,0,1,
+  1,1,1,1,1
+  },
+  { // 1
+  0,0,0,0,0,
+  0,0,0,0,0,
+  1,1,1,1,1
+  },
+  { // 2
+  1,1,1,0,1,
+  1,0,1,0,1,
+  1,0,1,1,1
+  },
+  { // 3
+  1,0,1,0,1,
+  1,0,1,0,1,
+  1,1,1,1,1
+  },
+  { // 4
+  0,0,1,1,1,
+  0,0,1,0,0,
+  1,1,1,1,1  
+  },
+  { // 5
+  1,0,1,1,1,
+  1,0,1,0,1,
+  1,1,1,0,1
+  },
+  { // 6
+  1,1,1,1,1,
+  1,0,1,0,1,
+  1,1,1,0,1
+  },
+  { // 7
+  0,0,0,0,1,
+  1,0,0,0,0,
+  1,1,1,1,1
+  },
+  { // 8
+  1,1,1,1,1,
+  1,0,1,0,1,
+  1,1,1,1,1
+  },
+  { // 9
+  0,0,1,1,1,
+  1,0,1,0,0,
+  1,1,1,1,1
+  }
+};
+
+
+const static uint8_t char_t[15] = {
+  1,0,0,0,0,
+  1,1,1,1,1,
+  1,0,0,0,0,
+};
+
+const static uint8_t char_c[15] = {
+  1,1,1,1,1,
+  1,0,0,0,1,
+  1,0,0,0,1
+};
+
+const static uint8_t time_delimiter[15] = {
+  0,0,0,0,0,
+  0,1,0,1,0,
+  0,0,0,0,0
+};
+
+const static CRGB::HTMLColorCode colors[35] = {
+  CRGB::Crimson,
+  CRGB::Red,
+  CRGB::DarkRed,
+  CRGB::DeepPink,
+  CRGB::OrangeRed,
+  CRGB::Orange,
+  CRGB::Gold,
+  CRGB::Yellow,
+  CRGB::Violet,
+  CRGB::Navy,
+  CRGB::RosyBrown,
+  CRGB::Fuchsia,
+  CRGB::MediumOrchid,
+  CRGB::BlueViolet,
+  CRGB::Purple,
+  CRGB::Indigo,
+  CRGB::DarkSlateBlue,
+  CRGB::GreenYellow,
+  CRGB::Lime,
+  CRGB::MediumSpringGreen,
+  CRGB::ForestGreen,
+  CRGB::DarkGreen,
+  CRGB::LightSeaGreen,
+  CRGB::Teal,
+  CRGB::Cyan,
+  CRGB::CadetBlue,
+  CRGB::SteelBlue,
+  CRGB::SkyBlue,
+  CRGB::Blue,
+  CRGB::DarkBlue,
+  CRGB::Chocolate,
+  CRGB::SaddleBrown,
+  CRGB::Maroon,
+  CRGB::DarkSlateGray,
+  CRGB::Brown
+};
